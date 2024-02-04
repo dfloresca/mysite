9d09cbf35d246242150470e7c2c65421459bd762
Daniel Floresca <dfloresca@gmail.com>
2024-02-04 07:08:09 -0800

    feat: added details, results and vote view, created index.html inside of templates/polls and rendered index

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from datetime import datetime, date, timedelta
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-load_dotenv()
-from bson import ObjectId
-
+from .models import Question
 
 # Create your views here.
 # views = routes
 def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
 
 
-    
+
+# from pymongo import MongoClient
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# from bson import ObjectId
+
 # client = MongoClient(os.getenv('MONGO_URI'))
 # db = client['mysite']
 # question = db.polls_question
@@ -81,5 +99,3 @@ def index(request):
 
 # # TODO Delete a question
 # # 
-
-    return HttpResponse("Hello, world. You're at the polls index.")
