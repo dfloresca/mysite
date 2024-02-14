5c44b9002145d0d675757b7142de7e52930c4572
Daniel Floresca <dfloresca@gmail.com>
2024-02-13 20:39:47 -0800

    feat: added auth imports to views.py, created base.html

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+# imports for authentication
+from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import  User
+from django.contrib.auth import  login , logout, authenticate
+from django.contrib.auth.decorators import  login_required
+from django.utils.decorators import  method_decorator
 
 def home(request):
     return render(request, "mysite/index.html")
     
+def login(request):
+    """How we login to our app"""
